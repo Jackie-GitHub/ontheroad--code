a7a126e9f25181e378a3f9ccd42847f175b93b2f
Jackie-GitHub <jackie.xuq@gmail.com>
2020-08-11 17:21:05 -0400

    remove used file: openWeather.js

--- src/axios/openWeather.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const KEY = 'd16369127b2966c047ff4c8ffe052bb9';
- 
-export default axios.create({
-    baseURL:'https://api.openweathermap.org/data/2.5',
-    params:{
-        appid:KEY}
-});
-
